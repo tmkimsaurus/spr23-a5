0cc19784da8ff8f508cc7b7a04de82f47117ad31
Matthew Kim <tmkim1216@gmail.com>
2023-06-04 19:14:59 -0700

    More files

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,7 +3,12 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
+# Checks that the program outputs "8" for an input of "4 + 4"
+assert run("4 + 4").output == "8"
+# Checks that the program outputs "0" for an input of "2 / 5"
+assert run("2 / 5").output == "0"
+# Checks that the program fails (correctly errors) for input "2+5"
+assert run("2+5").exit_status != 0
 
 ###
 
